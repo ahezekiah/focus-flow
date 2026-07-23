db16512a20635888d9c8cd13461ed69268194e1e
MrpZellen <158220937+MrpZellen@users.noreply.github.com>
2026-07-22 19:04:28 -0600

    Focusflow baseline

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+          <h1>Welcome to FocusFlow!</h1>
+          <h1>Site is a WIP, come back later!</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
