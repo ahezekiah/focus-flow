df1d3c19e5045f1268cf17bc1512d3ba82f8e3cd
MrpZellen <158220937+MrpZellen@users.noreply.github.com>
2026-08-03 19:41:26 -0600

    attempt 1, integrating amanda ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
+import Dashboard from './Dashboard'
 
 function App() {
-
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dash" element={<Dashboard />} />
+    </Routes>
   )
 }
 
